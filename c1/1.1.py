9261bfa61282dc163dbd63e1cf22fdaada9c0a65
James <inpuca@126.com>
2020-03-15 23:27:22 +0800

    今日份提交

--- c1/1.1.py
+++ c1/1.1.py
@@ -1,5 +1,6 @@
 import numpy as np
-A=np.array([1,2,3,4])
+A=np.array([[1,2,3,4]])
+print(A)
 A_t=A[:,np.newaxis]
 print(A_t)
-#print(A_t.shape)
+print(A_t.shape)
--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
-print("222")
-print("333")
+import numpy as np
+a = np.arange(24)
+print(a)
+print(a.ndim)  # a 现只有一个维度
+# 现在调整其大小
+b = a.reshape(2, 4, 3)  # b 现在拥有三个维度
+print(b)
+print(b.ndim)
+
+a = np.array([[1,2,3],[4,5,6]])
+print(a)
+print(a.shape)
